58407015e0e8a0ae2c7c65356aef0505ccb8c756
raiderjack <44075240+raiderjack@users.noreply.github.com>
2019-03-08 10:36:33 -0800

    Add files via upload

---
+++ NICKOLAS.py
@@ -0,0 +1,3 @@
+text = open("harrypotter1.txt").read() # the name of the file, relative to where the script is
+
+print(text) # Outputs: the entire text
---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello Python, nice to meet you.")
+type nul > hello.py
+echo print("Hello Python, nice to meet you.") > hello.py
+cd
+python hello.py
